4c00f101b443b53183eb573887ee57fdcf379dc9
mshayan3 <mshayan38@protonmail.ch>
2023-12-06 15:39:13 +0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/main.dart

--- lib/Screens/AdminScreens/admin_main_page.dart
+++ lib/Screens/AdminScreens/admin_main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 import '../../Services/AuthentactionRepository/authentication_repository.dart';
 
@@ -13,39 +14,28 @@ class _AdminPanelState extends State<AdminPanel> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Admin Panel'),
-      ),
-      body: Row(
-        children: [
-          // Sidebar
-          Container(
-            width: 200,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () => AuthenticationRepository.instance.logOut(),
-                  // Sign out the userawait _auth.signOut();
-                  // Navigate back to the sign-in screen (you can replace it with your desired destination)
-
-                  child: const Text('Sign Out'),
-                ),
-                _buildSidebarItem(0, Icons.dashboard, 'Dashboard'),
-                _buildSidebarItem(1, Icons.person, 'User Management'),
-                // Add more sidebar items as needed
-              ],
-            ),
-          ),
-          // Main content
-          Expanded(
-            child: _buildPage(),
-          ),
+      bottomNavigationBar: GNav(
+        backgroundColor: Colors.lightGreenAccent,
+        color: Colors.black,
+        activeColor: Colors.black,
+        tabBackgroundColor: Colors.lightGreen,
+        gap: 0,
+        selectedIndex: _selectedIndex, // Add this line to set the selected index
+        onTabChange: (index) {
+          // Add this function to handle tab changes
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        tabs: const [
+          GButton(icon: Icons.home, ),
+          GButton(icon: Icons.verified_user, ),
+          GButton(icon: Icons.tour,  ),
+          GButton(icon: Icons.payment,  ),
+          GButton(icon: Icons.logout, ),
         ],
       ),
+
     );
   }
 
