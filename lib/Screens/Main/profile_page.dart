4d3634129c9daefe28d27b7d9428fa846fc8e868
mshayan3 <mshayan38@protonmail.ch>
2023-12-12 14:16:03 +0500

    profile page updated
    Signed-off-by: mshayan3 <mshayan38@protonmail.ch>

--- lib/Screens/Main/profile_page.dart
+++ lib/Screens/Main/profile_page.dart
@@ -4,11 +4,7 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:travel_ease_fyp/Models/User/user_model.dart';
-import 'package:travel_ease_fyp/Services/AuthentactionRepository/authentication_repository.dart';
-import '../../Services/UserRepository/user_repository.dart';
 import 'package:path/path.dart' as path;
 
 class ProfilePage extends StatefulWidget {
@@ -24,141 +20,230 @@ class _ProfilePageState extends State<ProfilePage> {
   final TextEditingController _phoneNoController = TextEditingController();
   final TextEditingController _cnicController = TextEditingController();
 
+  String _profileImageUrl =
+      'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Profile Page',
-            style: TextStyle(
-              color: Colors.black,
-              letterSpacing: 1.5,
-              fontWeight: FontWeight.bold,
-              fontSize: 24,
-            ),
+      appBar: AppBar(
+        title: const Text(
+          'Profile Page',
+          style: TextStyle(
+            color: Colors.black,
+            letterSpacing: 1.5,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
           ),
         ),
-        body: FutureBuilder<DocumentSnapshot>(
-          // Fetch user details asynchronously
-          future: FirebaseFirestore.instance
-              .collection('users')
-              .doc(_auth.currentUser!.uid)
-              .get(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else if (!snapshot.hasData || snapshot.data == null) {
-              return const Center(child: Text('No user data available'));
-            } else {
-              // Data has been successfully fetched, display it
-              Map<String, dynamic>? userData =
-                  snapshot.data!.data() as Map<String, dynamic>?;
+      ),
+      body: FutureBuilder<DocumentSnapshot>(
+        // Fetch user details asynchronously
+        future: FirebaseFirestore.instance
+            .collection('users')
+            .doc(_auth.currentUser!.uid)
+            .get(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data == null) {
+            return const Center(child: Text('No user data available'));
+          } else {
+            // Data has been successfully fetched, display it
+            Map<String, dynamic>? userData =
+            snapshot.data!.data() as Map<String, dynamic>?;
 
-              if (userData != null) {
-                // Set the text in the controllers
-                _fullNameController.text = userData['fullName'] ?? '';
-                _usernameController.text = userData['username'] ?? '';
-                _emailController.text = userData['email'] ?? '';
-                _phoneNoController.text = userData['phoneNo'] ?? '';
-                _cnicController.text = userData['cnic'] ?? '';
+            if (userData != null) {
+              // Set the text in the controllers
+              _fullNameController.text = userData['fullName'] ?? '';
+              _usernameController.text = userData['username'] ?? '';
+              _emailController.text = userData['email'] ?? '';
+              _phoneNoController.text = userData['phoneNo'] ?? '';
+              _cnicController.text = userData['cnic'] ?? '';
 
-                return SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.all(16.0),
+              return SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () async {
+                        // Show a pop-up menu with options
+                        final result = await showMenu(
+                          context: context,
+                          position: const RelativeRect.fromLTRB(100, 200, 50, 0),
+                          items: const [
+                            PopupMenuItem(
+                              child: Text('Update'),
+                              value: 'change',
+                            ),
+                            PopupMenuItem(
+                              child: Text('Remove'),
+                              value: 'remove',
+                            ),
+                          ],
+                        );
+
+                        // Handle the selected option
+                        if (result != null) {
+                          if (result == 'change') {
+                            // Open the gallery to select an image
+                            File? imageFile = await _pickImage(ImageSource.gallery);
+                            if (imageFile != null) {
+                              // Upload the selected image to Firebase
+                              String imageUrl = await _uploadImage(
+                                  _auth.currentUser!.uid, imageFile);
+                              setState(() {
+                                _profileImageUrl = imageUrl;  // Use 'imageUrl' here
+                              });
+                            }
+                          } else if (result == 'remove') {
+                            // Set the current image as the profile picture
+                            // (You can implement the logic to remove the image from Firebase if needed)
+                          }
+                        }
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
                         child: CircleAvatar(
                           radius: 80,
-                          backgroundImage: NetworkImage(
-                            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHiP00HjutvKbqTXIv80K0FJvfejsOVzFagw&usqp=CAU',
-                          ),
+                          backgroundImage: NetworkImage(_profileImageUrl),
                         ),
                       ),
-                      ProfileField(
-                          label: 'Full Name',
-                          value: _fullNameController.text,
-                          controller: _fullNameController),
-                      ProfileField(
-                          label: 'Username',
-                          value: _usernameController.text,
-                          controller: _usernameController),
-                      ProfileField(
-                          label: 'Email',
-                          value: _emailController.text,
-                          controller: _emailController),
-                      ProfileField(
-                          label: 'Phone Number',
-                          value: _phoneNoController.text,
-                          controller: _phoneNoController),
-                      ProfileField(
-                          label: 'CNIC',
-                          value: _cnicController.text,
-                          controller: _cnicController),
-                      const SizedBox(height: 20),
-                      ElevatedButton(
-                        onPressed: () async {
-                          // Get the updated values from text fields
-                          String userId = _auth.currentUser!.uid;
-                          String fullName = _fullNameController.text;
-                          String username = _usernameController.text;
-                          String email = _emailController.text;
-                          String phoneNo = _phoneNoController.text;
-                          String cnic = _cnicController.text;
+                    ),
 
-                          // Reference to the user document in Firestore
-                          final userDoc = FirebaseFirestore.instance
-                              .collection('users')
-                              .doc(userId);
+                    ProfileField(
+                      label: 'Full Name',
+                      value: _fullNameController.text,
+                      controller: _fullNameController,
+                    ),
+                    ProfileField(
+                      label: 'Username',
+                      value: _usernameController.text,
+                      controller: _usernameController,
+                    ),
+                    ProfileField(
+                      label: 'Email',
+                      value: _emailController.text,
+                      controller: _emailController,
+                      readOnly: true,
+                    ),
+                    ProfileField(
+                      label: 'Phone Number',
+                      value: _phoneNoController.text,
+                      controller: _phoneNoController,
+                    ),
+                    ProfileField(
+                      label: 'CNIC',
+                      value: _cnicController.text,
+                      controller: _cnicController,
+                      readOnly: true,
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () async {
+                        // Get the updated values from text fields
+                        String userId = _auth.currentUser!.uid;
+                        String fullName = _fullNameController.text;
+                        String username = _usernameController.text;
+                        String email = _emailController.text;
+                        String phoneNo = _phoneNoController.text;
+                        String cnic = _cnicController.text;
 
-                          try {
-                            // Update the user's data in Firestore
-                            await userDoc.update({
-                              'fullName': fullName,
-                              'username': username,
-                              'email': email,
-                              'phoneNo': phoneNo,
-                              'cnic': cnic,
-                            });
+                        // Reference to the user document in Firestore
+                        final userDoc = FirebaseFirestore.instance
+                            .collection('users')
+                            .doc(userId);
 
-                            Fluttertoast.showToast(
-                                msg: 'Profile updated successfully');
-                          } catch (error) {
-                            print('Error updating user details: $error');
-                            // Handle the error as needed
-                          }
-                        },
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFFa2d19f),
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 20,
-                            vertical: 10,
-                          ),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25),
-                          ),
+                        try {
+                          // Update the user's data in Firestore
+                          await userDoc.update({
+                            'fullName': fullName,
+                            'username': username,
+                            'email': email,
+                            'phoneNo': phoneNo,
+                            'cnic': cnic,
+                          });
+
+                          Fluttertoast.showToast(
+                              msg: 'Profile updated successfully');
+                        } catch (error) {
+                          print('Error updating user details: $error');
+                          // Handle the error as needed
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFa2d19f),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                          vertical: 10,
                         ),
-                        child: const Text(
-                          'Save Changes',
-                          style: TextStyle(
-                            fontSize: 18,
-                            color: Colors.black,
-                          ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
                         ),
                       ),
-                      const SizedBox(height: 20),
-                    ],
-                  ),
-                );
-              } else {
-                return const Center(child: Text('User data is null'));
-              }
+                      child: const Text(
+                        'Save Changes',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                  ],
+                ),
+              );
+            } else {
+              return const Center(child: Text('User data is null'));
             }
-          },
-        ));
+          }
+        },
+      ),
+    );
   }
+
+  Future<File?> _pickImage(ImageSource source) async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: source);
+
+    if (pickedFile != null) {
+      return File(pickedFile.path);
+    } else {
+      return null;
+    }
+  }
+
+
+  Future<String> _uploadImage(String userId, File imageFile) async {
+    // Upload the image to Firebase Storage and return the download URL
+    Reference storageReference = FirebaseStorage.instance
+        .ref()
+        .child('profile_images')
+        .child('$userId.${path.extension(imageFile.path)}');
+
+    try {
+      // Upload the image file
+      await storageReference.putFile(imageFile);
+
+      // Get the download URL
+      String downloadURL = await storageReference.getDownloadURL();
+      print('Download URL: $downloadURL');
+
+      // Save the download URL in the user's data
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userId)
+          .update({'profilePicture': downloadURL});  // Use 'profilePicture' here
+
+      return downloadURL;
+    } catch (error) {
+      print('Error uploading image: $error');
+      return ''; // Handle the error as needed
+    }
+  }
+
+
 }
 
 class ProfileField extends StatelessWidget {
@@ -199,4 +284,3 @@ class ProfileField extends StatelessWidget {
     );
   }
 }
-
